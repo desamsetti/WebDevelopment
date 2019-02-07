ff30cfb52bcd8bbb6c7bdb3f794a7e329a1685e1
Harshith Desamsetti <dharshith1996@gmail.com>
2019-02-07 17:49:39 -0600

    Added Color Game Program

---
+++ JavaScript/Color Game/colorGame.js
@@ -0,0 +1,74 @@
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+//var pickedColor = colors[3];
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+colorDisplay.textContent = pickedColor;
+var h1 = document.querySelector("h1");
+
+for(var i=0;i<squares.length;i++)
+{
+	//Add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+
+	//Add click listeners to squares
+	squares[i].addEventListener("click",function(){
+		//grad color of clicked square
+		var clickedColor = this.style.backgroundColor;
+		//compare color to pickedColor
+		console.log(clickedColor,pickedColor);
+		if(clickedColor===pickedColor)
+		{			
+			messageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+		}
+		else
+		{		
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	})
+}
+
+
+function changeColors(color){
+	//loop through all squares
+	//change each color to match given color
+	for(var i=0;i<squares.length;i++){
+		//change each color to match given color
+		squares[i].style.backgroundColor = color;
+
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num)
+{
+	//make an array
+	var arr = []
+	//repeat num times
+	for(var i=0;i<num;i++){	
+		//get random color and push into arr
+		arr.push(randomColor());
+
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor(){
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256)
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256)
+	//pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256)
+	return "rgb(" + r +", "+ g + ", "+b+")";
+}

699c3c6531c53bea70a5dc4df090adc39a1a668f
Harshith Desamsetti <dharshith1996@gmail.com>
2019-02-10 22:50:00 -0600

    Added jQuery ToDoList Project

---
+++ JQuery/jQuery Effects/effects.js
@@ -0,0 +1,13 @@
+$("button").on("click",function(){
+	//$("div").slideUp(1000, function(){
+	//$("div").slideDown(1000, function(){
+	$("div").slideToggle(1000, function(){
+	//$("div").fadeToggle(1000, function(){
+	//$("div").fadeOut(1000, function(){
+	//$("div").fadeIn(1000, function(){
+		//console.log("Fade Completed!");
+		//$(this).remove();
+		console.log("SLIDE IS DONE!");
+	})	
+});
+
---
+++ JQuery/jQuery Exercise/jQueryExercise.js
@@ -0,0 +1,21 @@
+if(jQuery)
+{
+	alert("jQuery Connected");
+}
+else
+{
+	alert("jQuery not connected");
+}
+
+var purpleBackground = 
+{
+	backgroundColor:"purple"
+}
+
+$("div").css(purpleBackground)
+
+$(".highlight").css("width","200px")
+
+$("#third").css("border","2px solid orange")
+
+$("div:first-of-type").css("fontColor","pink")

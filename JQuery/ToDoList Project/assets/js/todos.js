651e0bcafd5870b939a7c0925475000157bf0fb2
Harshith Desamsetti <dharshith1996@gmail.com>
2019-02-11 22:00:02 -0600

    Added ToDo List Project using jQuery

--- JQuery/ToDoList Project/assets/js/todos.js
+++ JQuery/ToDoList Project/assets/js/todos.js
@@ -19,6 +19,13 @@ $("input[type='text'").keypress(function(event){
 		var todoText = $(this).val();
 		$(this).val("");
 		//create a new li and add to ul
-		$("ul").append("<li><span>X </span>"+todoText+"</li>")
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>"+todoText+"</li>")
 	}
 })
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});

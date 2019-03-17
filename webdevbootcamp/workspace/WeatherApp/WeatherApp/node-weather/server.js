874cc142738f6e45ef6313b1457947226124fb89
Harshith Desamsetti <dharshith1996@gmail.com>
2019-03-17 01:25:44 -0500

    Added Weather App, Created using the OpenWeatherMap API. Used express, body-parser, argv('yargs'), and request packages.'

---
+++ webdevbootcamp/workspace/WeatherApp/WeatherApp/node-weather/server.js
@@ -0,0 +1,51 @@
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const argv = require('yargs').argv;
+
+let request = require("request");
+let apiKey = '2230442326b83a53fc7a12b830ce157f';
+let city = argv.c || 'dekalb';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',function(req,res){
+    //res.send("Hello World");
+    res.render("index");
+});
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+
+
+app.listen(process.env.PORT,process.env.IP,function(err,res){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Started the Server");
+    }
+})

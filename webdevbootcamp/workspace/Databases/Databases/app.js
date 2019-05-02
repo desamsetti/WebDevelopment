c0746e543aa5ccb85f1bc47d0b54e65fa8637fc2
Harshith Desamsetti <dharshith1996@gmail.com>
2019-05-02 16:47:43 -0500

    Updated

--- webdevbootcamp/workspace/Databases/Databases/app.js
+++ webdevbootcamp/workspace/Databases/Databases/app.js
@@ -4,12 +4,14 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
 //mongoose.connect("mongodb://localhost:27017/superheroes_app",{useNewUrlParser:true});
-mongoose.connect("mongodb+srv://m001-student:ABCabc012@cluster0-05rxc.mongodb.net/test?retryWrites=true",{useNewUrlParser:true});
+mongoose.connect(
+  "mongodb+srv://m001-student:ABCabc012@cluster0-05rxc.mongodb.net/test?retryWrites=true",
+  { useNewUrlParser: true }
+);
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /*var superheroesArray = [{
     name:"Bruce Wayne",
@@ -19,24 +21,20 @@ app.use(bodyParser.urlencoded({extended:true}));
     image:"https://www.slashfilm.com/wp/wp-content/images/batman-animated-series-e1540567079198.jpg"
 }]*/
 
-
 //Connected to the heroes_app DB
-
 
 //Creating a Schema
 //A Superhero has a name, title, country, mainpower, image
 var superHeroSchema = mongoose.Schema({
-    name:String,
-    title:String,
-    country:String,
-    mainpower:String,
-    image:String
+  name: String,
+  title: String,
+  country: String,
+  mainpower: String,
+  image: String
 });
 
 //Compiling a Model from the Schema
-var SuperHero = mongoose.model("SuperHero",superHeroSchema);
-
-
+var SuperHero = mongoose.model("SuperHero", superHeroSchema);
 
 /*SuperHero.create({
     name:"Clark Kent",
@@ -54,70 +52,71 @@ var SuperHero = mongoose.model("SuperHero",superHeroSchema);
     }
 });*/
 
+app.get("/", function(req, res) {
+  res.render("landing");
 
-
-
-app.get("/",function(req,res){
-    res.render("landing");
-      
-    //res.send("Welcome to the Landing Page!");
+  //res.send("Welcome to the Landing Page!");
 });
 
-
 //INDEX - Show all superheroes
-app.get("/superheroes",function(req,res){
-    SuperHero.find({},function(err,allSuperHeroesFound){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("index",{allSuperheroes: allSuperHeroesFound});
-        }
-    });
+app.get("/superheroes", function(req, res) {
+  SuperHero.find({}, function(err, allSuperHeroesFound) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { allSuperheroes: allSuperHeroesFound });
+    }
+  });
 });
 
 //CREATE - add new Superhero to DB
-app.post("/superheroes",function(req,res){
-    var name = req.body.name;
-    var title = req.body.title;
-    var country = req.body.country;
-    var mainpower = req.body.mainpower;
-    var image = req.body.image;
-    
-    var newSuperHeroObject = {name:name,title:title,country:country,mainpower:mainpower,image:image};
-    
-    SuperHero.create(newSuperHeroObject,function(err,newlyCreated){
-        if(err){
-            console.log(err);
-        }else{
-            console.log("A New SuperHero has been inserted into the DB");
-            console.log(newlyCreated);
-            res.redirect("/superheroes");
-        }
-    });
-    
+app.post("/superheroes", function(req, res) {
+  var name = req.body.name;
+  var title = req.body.title;
+  var country = req.body.country;
+  var mainpower = req.body.mainpower;
+  var image = req.body.image;
+
+  var newSuperHeroObject = {
+    name: name,
+    title: title,
+    country: country,
+    mainpower: mainpower,
+    image: image
+  };
+
+  SuperHero.create(newSuperHeroObject, function(err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("A New SuperHero has been inserted into the DB");
+      console.log(newlyCreated);
+      res.redirect("/superheroes");
+    }
+  });
 });
 
 //NEW - Show form to creaate new Superhero
-app.get("/superheroes/new",function(req,res){
-    res.render("new.ejs");
+app.get("/superheroes/new", function(req, res) {
+  res.render("new.ejs");
 });
 
-
 //SHOW - Shows more info about one Superhero
-app.get("/superheroes/:id",function(req, res){
-    //find the campground with provided ID
-    SuperHero.findById(req.params.id,function(err,foundSuperHero){
-        if(err){
-            console.log(err);
-        }else{
-            //render show template with that campground
-            console.log(foundSuperHero.name);
-            res.render("show",{superhero:foundSuperHero});
-        }
-    });
-    //render show template with that campground
-})
-
-app.listen(process.env.PORT,process.env.IP,function(req,res){
-    console.log("SuperHeroes server has started!");
+app.get("/superheroes/:id", function(req, res) {
+  //find the campground with provided ID
+  SuperHero.findById(req.params.id, function(err, foundSuperHero) {
+    if (err) {
+      console.log(err);
+    } else {
+      //render show template with that campground
+      console.log(foundSuperHero.name);
+      res.render("show", { superhero: foundSuperHero });
+    }
+  });
+  //render show template with that campground
+});
+//console.log(process.env.PORT);
+//app.listen(process.env.PORT,process.env.IP,function(req,res){
+app.listen(3500, process.env.IP, function(req, res) {
+  console.log("SuperHeroes server has started!");
 });
